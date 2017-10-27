f7b6e576f4f74793bdaac0d828bcd05edab212c0
Sakis Hrist <sakishrist@gmail.com>
2017-10-27 14:13:48 +0300

    Better checks for eof and available data

--- Stream.pm
+++ Stream.pm
@@ -3,9 +3,9 @@ package Stream;
 use 5.010;
 use strict;
 use warnings;
-use IO::Select;
 use Term::ReadKey;
 use Data::Dumper;
+use Fcntl qw(O_NONBLOCK O_RDONLY);
 
 sub new {
 	my $class = shift;
@@ -17,7 +17,6 @@ sub new {
 	             'data' => '',
 	             'file' => '',
 	             'cbreak' => $cbreak,
-	             'select' => IO::Select->new(),
 	           };
 
 	bless $self, $class;
@@ -28,57 +27,38 @@ sub new {
 		$fd = $thing;
 	} else {
 		$self->{file} = $thing;
-		open $fd, "<", $thing or die;
+		sysopen($fd, $thing, O_RDONLY|O_NONBLOCK) or die "Could not open $thing";
 	}
 
 	ReadMode ( 3, *$fd ) if ($cbreak);
 	$self->{fd} = $fd;
-	$self->{select}->add(\*$fd);
 	$self->{isopen} = 1;
 
 	return $self;
 }
 
-sub readLine() {
-	my $self = shift;
-
-	my $fd = $self->{fd};
-
-	if ( $self->{select}->can_read(0) ) {
-		# I should be able to read ...
-		if ( defined (my $d = readline(*$fd)) ) {
-			# I just read ...
-			$self->{data} .= $d;
-			return 1;
-		} else {
-			$self->{isopen} = 0;
-		}
-	}
-
-	# Apparently no data is waiting to be read
-	return 0;
-}
-
 sub read() {
 	my $self = shift;
-
 	my $fd = $self->{fd};
-	#ReadMode ( 3, *$fd ) if ($self->{cbreak});
-	while ( $self->{select}->can_read(0) ) {
-		# I should be able to read ...
-		my $d;
-		if ( sysread(*$fd, $d, 1024) ) {
-			#print STDERR "Reading " . (length $d) . " bytes ...\n";
-			# I just read ...
-			$self->{data} .= $d;
-		} else {
-			# Oops ... it seems it's closed
-			last;
-		}
+	my $d;
+	my $res;
+
+	print STDERR "Attempting read from ".$self->{file}."...\n";
+
+	while (1) {
+		$res = sysread(*$fd, $d, 1024);
+		last unless (defined ($res) && $res != 0);
+		print STDERR "I read ". (length $d) . " bytes\n";
+		$self->{data} .= $d;
 	}
 
-	# Apparently no data is waiting to be read
-	return 1;
+	if ( ! defined ($res) && $!{EAGAIN}) {
+		print STDERR "Not blocking. Waiting for data ...\n";
+	} elsif (! defined ($res)) {
+		print STDERR "There was an error while reading: (" . ($!+0) . ") $!\n";
+	} elsif ($res == 0) {
+		print STDERR "We are at eof\n";
+	}
 }
 
 sub getData() {
